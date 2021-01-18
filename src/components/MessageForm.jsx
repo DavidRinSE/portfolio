d1c2a7a0b1f87dcbf292d32abaa9d117e6960d0b
DavidRinSE <davidrichardsonin@gmail.com>
2021-01-17 23:50:31 -0500

    Added contact form functionality

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {makeStyles} from "@material-ui/styles"
 import {TextField, Button} from "@material-ui/core"
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { useForm } from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
     form: {
@@ -41,13 +43,72 @@ const useStyles = makeStyles((theme) => ({
 
 const MessageForm = (props) => {
     const classes = useStyles()
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+    const [formSuccess, setFormSuccess] = useState(false)
+    const [formFail, setFormFail] = useState(false)
+    const { register, handleSubmit, errors } = useForm();
+
+    const submitForm = (data) => {
+        setButtonDisabled(true)
+        setFormSuccess(false)
+        setFormFail(false)
+        const url = 'https://upqa7matc6.execute-api.us-east-2.amazonaws.com/production/submit' // AWS Lambda function to email me form submissions
+        try {
+            fetch(url, {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(data)
+            }).then(res => res.json()).then(res => {
+                console.log(res)
+                setButtonDisabled(false)
+                if(res.message === 'Email sent successfully') {
+                    setFormSuccess(true)
+                } else {
+                    setFormFail(true)
+                }
+            });
+        } catch (err) {
+            console.log(err)
+            setFormFail(true)
+            setButtonDisabled(false)
+        }
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={handleSubmit(submitForm)}>
             <h3>Send Me a Message</h3>
-            <TextField id="email" label="Your Email" className={classes.root}/>
-            <TextField id="name" label="Name" />
-            <TextField id="message" label="Message" multiline rows={2}/>
-            <Button variant="contained" color="primary" disabled>Send</Button>
+            <TextField 
+                name="email"
+                label="Your Email"
+                className={classes.root}
+                inputRef={register({ 
+                    required: true,
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "Invalid email address"
+                    }
+                })}
+                error={errors.email}
+                helperText={errors.email && (errors.email.message ? errors.email.message : "Required field")}
+            />
+            <TextField name="name" label="Name" inputRef={register({ required: true })} error={errors.name} helperText={errors.name ? 'Required field': ''}/>
+            <TextField name="message" label="Message" multiline rows={3} inputRef={register({ required: true })} error={errors.message} helperText={errors.message ? 'Required field': ''}/>
+            <Button variant="contained" color="primary" disabled={buttonDisabled} type="submit">Send</Button>
+            {formSuccess && <Alert severity="success">
+                <AlertTitle>Success</AlertTitle>
+                Message sent successfully!
+            </Alert>}
+            {formFail && <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                There was a problem sending this message. Please try again, or use my contact links.
+            </Alert>}
         </form>
     )
 }
